136f62e7491b64fe3808b46cbcdfc37256253c88
Bradley Beddoes <bradleybeddoes@gmail.com>
2017-03-09 11:02:04 +1000

    Upgrade to AAF patched groovy 2.0.8 release
    We've manually patched the Groovy version for Groovy 7664, this commit
    brings it into the war for release.
    
    Note fallback to hard delete of existing 2.0.8 binary, for whatever
    reason the dependency resolution failed to do this on its own thus
    I had no option but the sledgehammer approach to ensure only the patched
    classes are in use.

--- virtualhome/grails-app/conf/BuildConfig.groovy
+++ virtualhome/grails-app/conf/BuildConfig.groovy
@@ -19,6 +19,8 @@ grails.project.dependency.resolution = {
   repositories {
     inherits true
 
+    flatDir name:"aaf-patched-groovy", dirs:"../aaf-patched-groovy/target/libs"
+
     grailsPlugins()
     grailsHome()
     grailsCentral()
@@ -33,6 +35,7 @@ grails.project.dependency.resolution = {
   }
 
   dependencies {
+    compile "org.codehaus:groovy-all:2.0.8+aaf.groovy7664"
     compile "commons-collections:commons-collections:3.2.2"
     compile "com.google.guava:guava:14.0"
 
